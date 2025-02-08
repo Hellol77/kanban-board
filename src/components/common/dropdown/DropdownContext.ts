888359a500f67d3ab971e2c71e9ba0f45591c49e
DongHyeonWon <dhe7700@naver.com>
2025-02-09 00:33:59 +0900

    fix: github cache 삭제

--- src/components/common/dropdown/DropdownContext.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-
-export const DropdownContext = createContext<{
-  isDropdownOpen: boolean;
-  toggleDropdown: () => void;
-}>({ isDropdownOpen: false, toggleDropdown: () => {} });
